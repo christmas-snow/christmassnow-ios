62b5514eca25703e9328c900f579990d41d692de
Oleg Derevenetz <oleg.derevenetz@gmail.com>
2017-12-14 21:58:28 +0000

    Updated translations.

--- iOS/christmassnow/translations/christmassnow_it.ts
+++ iOS/christmassnow/translations/christmassnow_it.ts
@@ -5,22 +5,22 @@
     <name>HelpDialog</name>
     <message>
         <source>Christmas Snow Scenes</source>
-        <translation type="unfinished"></translation>
+        <translation>Scene di neve di Natale</translation>
     </message>
     <message>
         <source>Have fun with a variety of Christmas Snow Scenes. Press Settings button to select a scene. Shake your phone to cause a snowfall. Control wind speed and direction by sliding your finger across a screen. Double tap on screen to hide buttons or show them again.</source>
-        <translation type="unfinished"></translation>
+        <translation>Divertiti con una varietà di scene sulla neve di Natale. Premere il pulsante Impostazioni per selezionare una scena. Scuoti il telefono per provocare una nevicata. Controlla la velocità e la direzione del vento facendo scorrere il dito su uno schermo. Tocca due volte lo schermo per nascondere i pulsanti o mostrarli di nuovo.</translation>
     </message>
 </context>
 <context>
     <name>PurchaseDialog</name>
     <message>
         <source>Purchase full version</source>
-        <translation type="unfinished"></translation>
+        <translation>Acquista la versione completa</translation>
     </message>
     <message>
         <source>Restore purchases</source>
-        <translation type="unfinished"></translation>
+        <translation>Ripristinare gli acquisti</translation>
     </message>
 </context>
 </TS>
