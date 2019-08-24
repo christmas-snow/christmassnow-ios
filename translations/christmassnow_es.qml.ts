def56645b2cf11d8efc4e0a6f1d777236f3b67eb
Oleg Derevenetz <oleg.derevenetz@gmail.com>
2019-08-24 17:07:11 +0300

    Update translations.

--- translations/christmassnow_es.qml.ts
+++ translations/christmassnow_es.qml.ts
@@ -2,14 +2,32 @@
 <!DOCTYPE TS>
 <TS version="2.1" language="es_ES">
 <context>
+    <name>AdMobConsentDialog</name>
+    <message>
+        <location filename="../qml/Core/Dialog/AdMobConsentDialog.qml" line="34"/>
+        <source>We keep this app free by showing ads. Ad network will &lt;a href=&quot;https://policies.google.com/technologies/ads&quot;&gt;collect data and use a unique identifier on your device&lt;/a&gt; to show you ads. &lt;b&gt;Do you allow to use your data to tailor ads for you?&lt;/b&gt;</source>
+        <translation>Mantenemos esta aplicación gratuita mostrando anuncios. La red publicitaria &lt;a href=&quot;https://policies.google.com/technologies/ads&quot;&gt;recopilar datos y utilizará un identificador único&lt;/a&gt; en su dispositivo para mostrarle anuncios. &lt;b&gt;¿Permites usar tus datos para personalizar anuncios para ti?&lt;/b&gt;</translation>
+    </message>
+    <message>
+        <location filename="../qml/Core/Dialog/AdMobConsentDialog.qml" line="62"/>
+        <source>Yes, show me relevant ads</source>
+        <translation>Sí, muéstrame anuncios relevantes</translation>
+    </message>
+    <message>
+        <location filename="../qml/Core/Dialog/AdMobConsentDialog.qml" line="94"/>
+        <source>No, show me ads that are less relevant</source>
+        <translation>No, muéstrame anuncios que sean menos relevantes</translation>
+    </message>
+</context>
+<context>
     <name>HelpDialog</name>
     <message>
-        <location filename="../qml/Core/Dialog/HelpDialog.qml" line="114"/>
+        <location filename="../qml/Core/Dialog/HelpDialog.qml" line="71"/>
         <source>Christmas Snow Scenes</source>
         <translation>Escenas de nieve navideñas</translation>
     </message>
     <message>
-        <location filename="../qml/Core/Dialog/HelpDialog.qml" line="115"/>
+        <location filename="../qml/Core/Dialog/HelpDialog.qml" line="72"/>
         <source>Have fun with a variety of Christmas Snow Scenes. Press Settings button to select a scene. Shake your phone to cause a snowfall. Control wind speed and direction by sliding your finger across a screen. Double tap on screen to hide buttons or show them again.</source>
         <translation>Diviértete con una variedad de escenas navideñas de nieve. Presione el botón Configuración para seleccionar una escena. Agite su teléfono para causar una nevada. Controle la velocidad y la dirección del viento deslizando el dedo por la pantalla. Toca dos veces en la pantalla para ocultar los botones o muéstralos de nuevo.</translation>
     </message>
@@ -17,12 +35,12 @@
 <context>
     <name>PurchaseDialog</name>
     <message>
-        <location filename="../qml/Core/Dialog/PurchaseDialog.qml" line="165"/>
+        <location filename="../qml/Core/Dialog/PurchaseDialog.qml" line="98"/>
         <source>Purchase full version</source>
         <translation>Comprar la versión completa</translation>
     </message>
     <message>
-        <location filename="../qml/Core/Dialog/PurchaseDialog.qml" line="240"/>
+        <location filename="../qml/Core/Dialog/PurchaseDialog.qml" line="149"/>
         <source>Restore purchases</source>
         <translation>Restaurar las compras</translation>
     </message>
@@ -30,7 +48,7 @@
 <context>
     <name>main</name>
     <message>
-        <location filename="../qml/main.qml" line="11"/>
+        <location filename="../qml/main.qml" line="13"/>
         <source>Snow Scenes</source>
         <translation>Nieve</translation>
     </message>
--- translations/christmassnow_ru.qml.ts
+++ translations/christmassnow_ru.qml.ts
@@ -2,14 +2,32 @@
 <!DOCTYPE TS>
 <TS version="2.1" language="ru_RU">
 <context>
+    <name>AdMobConsentDialog</name>
+    <message>
+        <location filename="../qml/Core/Dialog/AdMobConsentDialog.qml" line="34"/>
+        <source>We keep this app free by showing ads. Ad network will &lt;a href=&quot;https://policies.google.com/technologies/ads&quot;&gt;collect data and use a unique identifier on your device&lt;/a&gt; to show you ads. &lt;b&gt;Do you allow to use your data to tailor ads for you?&lt;/b&gt;</source>
+        <translation>Это приложение распространяется бесплатно за счет показа в нем рекламы. Рекламная сеть будет &lt;a href=&quot;https://policies.google.com/technologies/ads&quot;&gt;собирать данные и использовать уникальный идентификатор вашего устройства&lt;/a&gt; для того, чтобы показывать вам рекламные объявления. &lt;b&gt;Разрешаете ли вы использовать свои данные для выбора наиболее подходящих для вас рекламных объявлений?&lt;/b&gt;</translation>
+    </message>
+    <message>
+        <location filename="../qml/Core/Dialog/AdMobConsentDialog.qml" line="62"/>
+        <source>Yes, show me relevant ads</source>
+        <translation>Да, показывайте более подходящую рекламу</translation>
+    </message>
+    <message>
+        <location filename="../qml/Core/Dialog/AdMobConsentDialog.qml" line="94"/>
+        <source>No, show me ads that are less relevant</source>
+        <translation>Нет, показывайте менее подходящую рекламу</translation>
+    </message>
+</context>
+<context>
     <name>HelpDialog</name>
     <message>
-        <location filename="../qml/Core/Dialog/HelpDialog.qml" line="114"/>
+        <location filename="../qml/Core/Dialog/HelpDialog.qml" line="71"/>
         <source>Christmas Snow Scenes</source>
         <translation>Новогодние пейзажи</translation>
     </message>
     <message>
-        <location filename="../qml/Core/Dialog/HelpDialog.qml" line="115"/>
+        <location filename="../qml/Core/Dialog/HelpDialog.qml" line="72"/>
         <source>Have fun with a variety of Christmas Snow Scenes. Press Settings button to select a scene. Shake your phone to cause a snowfall. Control wind speed and direction by sliding your finger across a screen. Double tap on screen to hide buttons or show them again.</source>
         <translation>Наслаждайтесь живыми новогодними пейзажами. Нажмите кнопку Настройки для выбора фонового изображения. Потрясите телефон для того, чтобы вызвать снегопад. Меняйте скорость и направление ветра, проведя пальцем по экрану. Нажмите дважды на экран для того, чтобы спрятать кнопки или снова показать их.</translation>
     </message>
@@ -17,12 +35,12 @@
 <context>
     <name>PurchaseDialog</name>
     <message>
-        <location filename="../qml/Core/Dialog/PurchaseDialog.qml" line="165"/>
+        <location filename="../qml/Core/Dialog/PurchaseDialog.qml" line="98"/>
         <source>Purchase full version</source>
         <translation>Купить полную версию</translation>
     </message>
     <message>
-        <location filename="../qml/Core/Dialog/PurchaseDialog.qml" line="240"/>
+        <location filename="../qml/Core/Dialog/PurchaseDialog.qml" line="149"/>
         <source>Restore purchases</source>
         <translation>Восстановить покупки</translation>
     </message>
@@ -30,7 +48,7 @@
 <context>
     <name>main</name>
     <message>
-        <location filename="../qml/main.qml" line="11"/>
+        <location filename="../qml/main.qml" line="13"/>
         <source>Snow Scenes</source>
         <translation>Снег</translation>
     </message>
