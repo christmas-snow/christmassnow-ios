4ff5542e219c4e47993756f6b666fb73f700e4af
Oleg Derevenetz <oleg-derevenetz@yandex.ru>
2020-08-02 03:04:15 +0300

    Use autoloadEnabled = YES to reload ads. It looks like it should use original request with proper extras.

--- src/admobhelper.mm
+++ src/admobhelper.mm
@@ -14,7 +14,7 @@ const QString AdMobHelper::ADMOB_TEST_DEVICE_ID    (QLatin1String(""));
 
 namespace {
 
-constexpr NSTimeInterval AD_RELOAD_DELAY = 60.0;
+constexpr NSTimeInterval AD_RELOAD_ON_FAILURE_DELAY = 60.0;
 
 }
 
@@ -55,7 +55,7 @@ constexpr NSTimeInterval AD_RELOAD_DELAY = 60.0;
         BannerView = [[GADBannerView alloc] initWithAdSize:kGADAdSizeSmartBannerPortrait];
 
         BannerView.adUnitID                                  = AdMobHelper::ADMOB_BANNERVIEW_UNIT_ID.toNSString();
-        BannerView.autoloadEnabled                           = NO;
+        BannerView.autoloadEnabled                           = YES;
         BannerView.translatesAutoresizingMaskIntoConstraints = NO;
         BannerView.rootViewController                        = root_view_controller;
         BannerView.delegate                                  = self;
@@ -132,8 +132,6 @@ constexpr NSTimeInterval AD_RELOAD_DELAY = 60.0;
         AdMobHelperInstance->SetBannerViewHeight(qFloor(BannerView.frame.size.height + root_view_controller.view.safeAreaInsets.top
                                                                                      - status_bar_height));
     }
-
-    [self performSelector:@selector(loadAd) withObject:nil afterDelay:AD_RELOAD_DELAY];
 }
 
 - (void)adViewWillPresentScreen:(GADBannerView *)adView
@@ -157,7 +155,7 @@ constexpr NSTimeInterval AD_RELOAD_DELAY = 60.0;
 
     qWarning() << QString::fromNSString(error.localizedDescription);
 
-    [self performSelector:@selector(loadAd) withObject:nil afterDelay:AD_RELOAD_DELAY];
+    [self performSelector:@selector(loadAd) withObject:nil afterDelay:AD_RELOAD_ON_FAILURE_DELAY];
 }
 
 @end
